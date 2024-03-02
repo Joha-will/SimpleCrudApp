d08ba5b0212767a6ad5869f4e8916ca4b389a878
Joha-will <johawilsonatu@icloud.com>
2024-03-02 18:23:26 +0000

    Installed and connect to MongoDB database and configured environment variables

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-const express = require('express')
-const app = express()
+require('dotenv').config({ path: './.env' }) 
+const express = require("express");
+const mongoose = require('mongoose');
+const app = express();
 
-app.listen(3000, () => {
-    console.log("Serve is running on port 3000!")
-})
 
-app.get('/', (req, res) => {
-    res.send("Hello from node API")
-})
+
+app.get("/", (req, res) => {
+  res.send("Hello from Node API Server Updated");
+});
+
+mongoose.connect(process.env.CONNECTION_STRING)
+.then(() => {
+  console.log('Connected!');
+  app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+  });
+});
